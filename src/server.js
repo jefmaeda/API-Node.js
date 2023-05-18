852f0fd3fe475d1fde391226c99b962db4be7c09
Jef <jefersontamono@gmail.com>
2023-05-18 16:28:46 +0900

    feat(route params): get parameter of browser

--- src/server.js
+++ src/server.js
@@ -3,6 +3,14 @@ const express = require("express")
 //initialization express
 const app = express()
 
+//GET method
+app.get("/message/:id/:user", (request, response) =>{
+    //get parameter of browser (route params)
+    const {id, user} = request.params
+    //get response
+    response.send(`ID: ${id} this user name is :${user}`)
+})
+
 //port listener 
 const PORT = 3333
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
