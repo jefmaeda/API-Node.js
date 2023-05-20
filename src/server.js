d0bb15ccf34aaa27b8435b6a560cfcd56e3fd928
Jef <jefersontamono@gmail.com>
2023-05-20 15:59:34 +0900

    refactor

--- src/server.js
+++ src/server.js
@@ -1,20 +1,27 @@
 const express = require("express")
+const routes = require("./routes")
 
 //initialization express
 const app = express()
+//body type json
+app.use(express.json())
+app.use(routes)
 
 //GET method
-app.get("/message/:id/:user", (request, response) =>{
-    //get parameter of browser (route params)
-    const {id, user} = request.params
-    //get response
-    response.send(`ID: ${id} this user name is :${user}`)
-})
+// app.get("/message/:id/:user", (request, response) =>{
+//     //get parameter of browser (route params)
+//     const {id, user} = request.params
+//     //get response
+//     response.send(`ID: ${id} this user name is :${user}`)
+// })
 
-app.get("/users", (request, response) =>{
-    const {page,limit} = request.query
-    response.send(`Page: ${page}. Show: ${limit}`)
-})
+// app.get("/users", (request, response) =>{
+//     const {page,limit} = request.query
+//     response.send(`Page: ${page}. Show: ${limit}`)
+// })
+
+
+
 //port listener 
 const PORT = 3333
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
