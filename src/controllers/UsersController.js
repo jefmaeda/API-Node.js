eea270d24463e9470c9ff4d7bbc9e1010984a313
Jef <jefersontamono@gmail.com>
2023-05-28 05:24:25 +0900

    feat(put): update record enabel - password crypted

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,5 +1,5 @@
+const { hash } = require("bcryptjs")
 const AppError = require("../utils/AppError")
-
 const sqliteConnection = require("../database/sqlite")
 
 //vamos usar a class para agrupamento de functions
@@ -23,11 +23,42 @@ class UsersController {
             throw new AppError("This email is already in use.")
         }
 
+        const hashedPassword = await hash(password, 8)
+
         //insert into database
         await database.run(
-            "INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [name, email, password]
+            "INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [name, email, hashedPassword]
         )
         return response.status(201).json()
+    }
+
+    async update(request,response){
+        const { name, email } = request.body
+        const { id } = request.params
+
+        const database = await sqliteConnection()
+        const user = await database.get("SELECT * FROM users WHERE id = (?)", [id])
+
+        if(!user){
+            throw new AppError("User not found")
+        }
+
+        const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+        if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id){
+            throw new AppError("This email is already in use")
+        }
+
+        user.name = name
+        user.email = email
+
+        await database.run(
+        'UPDATE users SET name = ?, email = ?, updated_at = ? WHERE id = ?',
+        [user.name, user.email, new Date(), id]
+        )
+
+        // status 200 is default
+        return response.json()
     }
 }
 
