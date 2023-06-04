e245e64ce3c196ce3b05a2df6f5d2f20ccf84422
Jef <jefersontamono@gmail.com>
2023-06-04 10:34:39 +0900

    feat(drop): drop enable script

--- knexfile.js
+++ knexfile.js
@@ -8,6 +8,13 @@ module.exports = {
     connection: {
       filename: path.resolve(__dirname, "src", "database", "database.db")
     },
+    //drop enable script
+    pool: {
+      afterCreate: (conn, cb) => conn.run("PRAGMA foreign_keys = ON", cb)
+    },
+    migrations: {
+      directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
+    },
     useNullAsDefault: true
   }
 };
