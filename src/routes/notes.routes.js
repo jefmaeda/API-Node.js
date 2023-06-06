ae303c9fe028f1e5fd1b442916360297a2b4e612
Jef <jefersontamono@gmail.com>
2023-06-06 16:55:10 +0900

    feat(get id): get id in search bar - function show

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -5,6 +5,7 @@ const notesRoutes = Router()
 const notesController = new NotesController
 
 notesRoutes.post("/:user_id", notesController.create)
+notesRoutes.get("/:id",notesController.show)
 
 //export ro anywhere
 module.exports = notesRoutes
