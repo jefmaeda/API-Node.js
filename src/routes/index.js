6d7835b07301dff4ff60017e6b219dcdc6d50008
Jef <jefersontamono@gmail.com>
2023-06-06 16:56:46 +0900

    feat(notes routes)

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const { Router } = require("express")
 
 const usersRoutes = require("./users.routes")
+const notesRoutes = require("./notes.routes")
 
 const routes = Router()
-//when accessing users automatically goes to usersRoutes
+//when accessing users automatically goes to Routes
 routes.use("/users", usersRoutes)
+routes.use("/notes", notesRoutes)
 
 module.exports = routes
