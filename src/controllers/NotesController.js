e215457c48820387fc680ded7a32188bea3b06e6
Jef <jefersontamono@gmail.com>
2023-06-06 16:52:37 +0900

    feat(show) - note table - tags where note_ID - links where note_ID

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -35,6 +35,22 @@ class NotesController{
 
         response.json()
     }
+
+    async show(request, response){
+        const { id } = request.params
+
+        //get notes specific where id
+        const note = await knex("notes").where({id}).first()
+        //get tags specific noteid of id(request.params)
+        const tags = await knex("tags").where({note_id: id}).orderBy("name")
+        const links = await knex("links").where({note_id: id}).orderBy("created_at")
+
+        return response.json({
+            ...note,
+            tags,
+            links
+        })
+    }
 }
 
 module.exports = NotesController
